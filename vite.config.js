f14736f2836ecfc74dc292bf5d4d8573ef9fd1cf
Bálint Tóth <72336409+Baleszko@users.noreply.github.com>
2023-02-24 13:17:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio",
+  base: "/portfolio/",
   plugins: [
     react(),
     VitePluginFonts({
